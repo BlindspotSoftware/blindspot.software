f3bc53397da1b8d494b7f52102bb71957544c2c0
Jan Koob <jankoob@MBP-von-Jan.9e.network>
2023-03-15 09:31:26 +0100

    improved accessibility for buttons

--- src/js/scrollAnimation.js
+++ src/js/scrollAnimation.js
@@ -3,14 +3,13 @@ gsap.registerPlugin(ScrollTrigger);
 let headlines = gsap.utils.toArray('.braces');
 
 headlines.forEach((headline) => {
-  // Set gap to opened state first
+  // Set gap to opened state first (JS is active)
   headline.style.setProperty('--gap', '1em');
 
   gsap.to(headline, {
     scrollTrigger: {
       trigger: headline,
       toggleActions: 'play complete none reset',
-      toggleClass: 'enable',
     },
     duration: 0.5,
     // Closed state
